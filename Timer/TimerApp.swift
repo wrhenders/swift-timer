ec2ff9e0575af1cec4fa4de96cd168aba1905057
wrhenders <wrhenders@gmail.com>
2022-11-10 15:31:00 -0600

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Ryan Henderson on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
